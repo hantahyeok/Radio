c7505435c3e98621459f98a6d895fd4398b33924
hantahyeok <hantahyeok@naver.com>
2023-06-09 16:41:30 +0900

    라디오...

--- app/src/main/java/com/hdk/radio/MainActivity.java
+++ app/src/main/java/com/hdk/radio/MainActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -24,12 +25,19 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String streamUrl = "http://radio.linnrecords.com:8000/stream"; // 라디오 API를 통해 얻은 스트림 URL
+                String streamUrl = "http://ebsonairiosaod.ebs.co.kr/fmradiobandiaod/bandiappaac/playlist.m3u8"; // 라디오 API를 통해 얻은 스트림 URL
                 MediaPlayer mediaPlayer = new MediaPlayer();
                 try {
                     mediaPlayer.setDataSource(streamUrl);
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mp.setVolume(1.0f, 1.0f);
+                            mp.start(); // 준비가 완료되면 재생 시작
+                            Toast.makeText(MainActivity.this, "음악 플레이중...", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                     mediaPlayer.prepareAsync();
-                    mediaPlayer.start(); // 재생 시작
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
